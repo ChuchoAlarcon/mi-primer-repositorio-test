1a5602609db9cd58927f7cf1d2ee5a0b3f8908d5
jesus <alarcon@gmail,com>
2020-07-29 14:49:04 +0000

    iniciando con git

---
+++ app/src/main/java/jesus/alarcon/test/test.java
@@ -0,0 +1,13 @@
+package jesus.alarcon.test;
+
+public class test {
+
+
+    public static void imprimir(){
+        System.out.println("Emi");
+    }
+
+    public static void main(String[] args) {
+        imprimir();  
+    }
+}
